3f31611c02b92af4adbe7e34e8f487e6be933438
JorgeSDiaz <saenzdiazj0@gmail.com>
2024-05-02 10:06:24 -0500

    refactor(brands): :recycle: use readonly properties

--- src/dtos/brand.dto.ts
+++ src/dtos/brand.dto.ts
@@ -4,11 +4,11 @@ import { IsString, IsNotEmpty } from 'class-validator';
 export class CreateBrandDto {
   @IsString()
   @IsNotEmpty()
-  name: string;
+  readonly name: string;
 
   @IsString()
   @IsNotEmpty()
-  description: string;
+  readonly description: string;
 }
 
 export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
